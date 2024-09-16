e7d5577d5fedff4cf26d7d39cd46f59e88896694
Zaher Zeinni <zaherzeinni@gmail.com>
2024-09-17 01:10:59 +0300

    please blogs and services read 5min and view dont delete them i need them

--- pages/admin/blog/create.tsx
+++ pages/admin/blog/create.tsx
@@ -34,7 +34,7 @@ const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 const uploadApi = "https://file-uploader-red.vercel.app";
 
 const colorList = ['#001f3f', '#0074D9', '#7FDBFF', '#39CCCC', '#3D9970', '#2ECC40', '#01FF70', '#FFDC00', '#FF851B', '#FF4136', '#85144b', '#F012BE', '#B10DC9', '#111111', '#AAAAAA', '#DDDDDD', '#FFFFFF','#1C00ff00'];
-//color list
+
 
 
 const modules = {
